263cffba3a134560b1d84f42155f49f4b94c66f3
AlexanderGolys <alexandergolys@gmail.com>
2021-06-25 20:05:03 +0200

    Initial commit

---
+++ basic_func.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from PIL import Image
+
+
+class Frame:
+    """
+    Single animation frame or unitary image.
+    """
+
+    def __init__(self, res):
+        """
+        Arguments
+        _________
+        res : tuple of ints
+            
+        """
