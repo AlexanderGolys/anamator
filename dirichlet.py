c863de81efa4af5734afb27fb525700b96dd0e72
mirein-a <68952159+mirein-a@users.noreply.github.com>
2021-08-19 16:43:48 +0200

    Update dirichlet.py

--- dirichlet.py
+++ dirichlet.py
@@ -5,18 +5,11 @@ import numpy as np
 import src.anamator.basic_func as basic_func
 import src.anamator.objects as objects
 
-from random import uniform, randint
+from random import uniform, randint, choices
 
-
-def partition_fractal(no_of_points, no_of_fixed, interval_length):
-    fixed = [uniform(0, .4*interval_length) for _ in range(no_of_fixed)] + [uniform(.6*interval_length, 1) for _ in range(no_of_fixed)]
-    random_points = [uniform(0, .4*interval_length) for _ in range(no_of_points - no_of_fixed)] + [uniform(.6*interval_length, 1) for _ in range(no_of_points - no_of_fixed)]
-    random_points.sort()
-    fixed.sort()
-    before_zoom = [0.45*interval_length+p/10 for p in fixed]
-    for_zoom = [0.45*interval_length+p/10 for p in random_points]
-    return fixed, random_points
-
+# todo:
+# (1) enhance random points
+# (2) choose the equal amount of 1s and 0s for values
 
 def dirichlet_function_but_enhanced(fixed, random_points):
     values_fixed = [randint(0, 1) for _ in range(len(fixed))]
@@ -24,31 +17,54 @@ def dirichlet_function_but_enhanced(fixed, random_points):
     return values_fixed, values_random
 
 
-def split_object_list(centers, radius):
+def split_object_list(centers, radius, limit):
 
     too_close_centers = []
-    i = 1
-    while i < len(centers)-1:
-        if centers[i][1] == centers[i + 1][1]:
-            if abs(centers[i][0] - centers[i + 1][0]) < 2 * radius:
-                print(centers[i], centers[i + 1])
-                too_close_centers.append(centers[i + 1])
-                del centers[i]
-                if i == len(centers):
-                    del centers[i]
-            else:
-                i += 1
+
+    for k, ctr in enumerate(centers):
+        if k < len(centers)-1:
+            tmp = centers[k+1]
         else:
-            i += 1
+            break
+        if ctr[1] == tmp[1]:
+            if abs(ctr[0] - tmp[0]) < 2 * radius:
+                too_close_centers.append(tmp)
 
-    return [centers, too_close_centers]
+    centers = list(set(centers)-set(too_close_centers))
+    centers.sort()
+    complete = [centers, too_close_centers]
+
+    if limit > 0:
+        more_split = split_object_list(too_close_centers, radius, limit-1)
+        if more_split is not None:
+            if len(more_split) == 2:
+                more_ctrs, even_more_ctrs = more_split
+                if len(even_more_ctrs) > 0:
+                    complete.pop()
+                    complete.append(more_ctrs)
+                    complete.append(even_more_ctrs)
+                else:
+                    return None
+
+    return complete
 
 
-def frame_gen(no_of_points, no_of_fixed, interval):
+def partition_fractal(no_of_points, no_of_fixed, interval_length):
+    fixed = [uniform(0, .4*interval_length) for _ in range(no_of_fixed)] + [uniform(.6*interval_length, interval_length) for _ in range(no_of_fixed)]
+    random_points = [uniform(0, .4*interval_length) for _ in range(no_of_points - no_of_fixed)] + [uniform(.6*interval_length, interval_length) for _ in range(no_of_points - no_of_fixed)]
+    random_points.sort()
+    fixed.sort()
+
+    before_zoom = [0.45*interval_length+p/10 for p in fixed]
+    for_zoom = [0.45*interval_length+p/10 for p in random_points]
+    return fixed, random_points
+
+
+def single_frame(no_of_points, no_of_fixed, interval, radius, t, resolution=(3840, 2160)):
     fixed, random_points = partition_fractal(no_of_points, no_of_fixed, interval[1]-interval[0])
     values_fixed, values_random = dirichlet_function_but_enhanced(fixed, random_points)
     before_zoom = [0.45*(interval[1]-interval[0]) + p / 10*(interval[1]-interval[0]) for p in fixed]
-    # for_zoom = [0.45 + p / 10 for p in random_points]
+    # for_zoom = [0.45*(interval[1]-interval[0]) + p / 10*(interval[1]-interval[0]) for p in random_points]
 
     centers = list(zip(fixed + random_points + before_zoom, values_fixed + values_random + values_fixed))
 
@@ -62,7 +78,7 @@ def frame_gen(no_of_points, no_of_fixed, interval):
     settings_point_interior = {
         'sampling rate': 1,
         'thickness': 1,
-        'blur': 3,
+        'blur': 2,
         'color': 'gray',
         'blur kernel': 'box'
     }
@@ -70,24 +86,41 @@ def frame_gen(no_of_points, no_of_fixed, interval):
     frame = basic_func.OneAxisFrame((3840, 2160), 'black', 100, 100)
     frame.add_axis_surface(x_bounds=(interval[0]-.1, interval[1] + .1), y_bounds=(-.3, 2))
     frame.blit_axes(settings_axes, x_only=False)
-    list_of_centers = split_object_list([frame.axis_surface.transform_to_pixel_coordinates(ctr) for ctr in centers], 8)
-    before_zoom_points = [[objects.BitmapDisk(8, 'white', 1, padding=0) for _ in centers] for centers in list_of_centers]
-    frame.axis_surface.blit_distinct_bitmap_objects(centers, before_zoom_points, settings_point_interior)
-    # for pt in zip(centers, before_zoom_points):
-    #     frame.axis_surface.blit_distinct_bitmap_objects([pt[0]], [pt[1]], settings_point_interior)
+    list_of_centers = split_object_list([frame.axis_surface.transform_to_pixel_coordinates(ctr) for ctr in centers], 32, 5)
+    before_zoom_points = [[objects.BitmapDisk(radius, 'white', 1, padding=0) for _ in centers] for centers in list_of_centers]
+    for pts in zip(list_of_centers, before_zoom_points):
+        frame.axis_surface.blit_distinct_bitmap_objects(pts[0], pts[1], settings_point_interior, surface_coordinates=False)
+
+    # do wyjebania:
+    interval_points = [objects.BitmapDisk(radius, 'blue', 1, padding=0) for _ in range(0,interval[1]+1)] + [objects.BitmapDisk(radius, 'green', 1, padding=0) for _ in range(2)]
+    interval_centers = [(x, 0) for x in range(0,interval[1]+1)]+[(x, 0) for x in [.4*interval[1], .6*interval[1]]]
+    frame.axis_surface.blit_distinct_bitmap_objects(interval_centers, interval_points, settings_point_interior, surface_coordinates=True)
+    # koniec wyjebywania
+
     frame.blit_axis_surface()
     frame.generate_png('dirichlet.png')
 
     return frame
 
 
-def animate_zoom():
-    pass
+def animate_zoom(no_of_points, no_of_fixed, radius, interval,speed, resolution, filename='dirichlet_function.mp4',
+                             id_='dirichlet', slow=False):
+    def frame_gen(t):
+        return single_frame(no_of_points, no_of_fixed, interval, radius, t, resolution=resolution)
+
+    animator = basic_func.SingleAnimation(frame_gen, objects.PredefinedSettings.slow_differential) \
+        if slow else basic_func.SingleAnimation(frame_gen, objects.PredefinedSettings.fast_differential)
+    settings = {
+        'fps': 24,
+        'resolution': resolution,
+        'duration': 1
+    }
+    animator.render(filename, settings, True, id_, speed=speed)
 
 if __name__ == '__main__':
     # print(random_points(30))
-    # frame_gen(15,5,(0,3))
+    frame_gen(50,25,(0,3))
     # print('dupa')
-    ctrs = [(1,0), (1.5,0), (2,0), (3,0), (3.7,0),(5,0), (5.6,0), (6,0), (8,0)]
-    r = .4
-    print(split_object_list(ctrs,r))
+    # ctrs = [(1,0), (1.5,0), (2,0), (3,0), (3.7,0),(5,0), (5.6,0), (6,0), (8,0), (8,1)]
+    # r = .4
+    # print(split_object_list(ctrs,r,5))
